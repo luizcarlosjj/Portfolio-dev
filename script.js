14dfdfb6846d4da3b42065e19b01b904a3220614
Luiz Carlos <dia.a.diajj@gmail.com>
2023-05-12 14:04:10 -0300

    javscript

---
+++ script.js
@@ -0,0 +1,36 @@
+function html() {
+  var a = window.document.getElementById('mod1')
+  a.style.background = '#dc3545'
+  a.style.transition = '1s'
+}
+
+function css() {
+  var b = window.document.getElementById('mod2')
+  b.style.background = '#dc3545'
+  b.style.transition = '1s'
+}
+function js() {
+  var c = window.document.getElementById('mod3')
+  c.style.background = '#dc3545'
+  c.style.transition = '1s'
+}
+function word() {
+  var d = window.document.getElementById('mod4')
+  d.style.background = '#dc3545'
+  d.style.transition = '1s'
+}
+
+function sair () {
+  var a = window.document.getElementById('mod1')
+  var b = window.document.getElementById('mod2')
+  var c = window.document.getElementById('mod3')
+  var d = window.document.getElementById('mod4')
+  a.style.background = '#151515'
+  a.style.transition = '1s'
+  b.style.background = '#151515'
+  b.style.transition = '1s'
+  c.style.background = '#151515'
+  c.style.transition = '1s'
+  d.style.background = '#151515'
+  d.style.transition = '1s'
+}
